84980ba97202fac87ba4045512d9568949fc660d
monaiem2022 <95927663+monaiem2022@users.noreply.github.com>
2023-01-14 21:20:53 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,277 @@
+
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+let four = document.getElementById('four');
+let five = document.getElementById('five');
+let six = document.getElementById('six');
+let seven = document.getElementById('seven');
+
+let details = document.getElementById('details');
+let mainImage = document.getElementById('mainImage');
+let marie60 = document.getElementById('marie60');
+
+let mainCartoon = document.getElementById('mainCartoon');
+let mainBox = document.getElementById('mainBox');
+let mainDiscount = document.getElementById('mainDiscount');
+let mainPrice = document.getElementById('mainPrice');
+let insideCrtoon = document.getElementById('insideCrtoon');
+let totalDiscount = document.getElementById('totalDiscount');
+let mainTitle = document.getElementById('mainTitle');
+
+
+let biscuit110 = document.getElementById('biscuit110');
+let biscuit110_1 = document.getElementById('biscuit110_1');
+let biscuit110_2 = document.getElementById('biscuit110_2');
+let biscuit110_3 = document.getElementById('biscuit110_3');
+let biscuit110_4 = document.getElementById('biscuit110_4');
+let biscuit110_5 = document.getElementById('biscuit110_5');
+let totalBiscuit110 = document.getElementById('totalBiscuit110');
+let biscuit110Title = document.getElementById('biscuit110Title');
+
+
+
+
+let wafer = document.getElementById('biscuit110');
+let wafer_1 = document.getElementById('wafer_1');
+let wafer_2 = document.getElementById('wafer_2');
+let wafer_3 = document.getElementById('wafer_3');
+let wafer_4 = document.getElementById('wafer_4');
+let wafer_5 = document.getElementById('wafer_5');
+let totalWafer = document.getElementById('totalWafer');
+let waferTitle = document.getElementById('waferTitle');
+
+
+
+let promo = document.getElementById('promo');
+let promo_1 = document.getElementById('promo_1');
+let Promo_2 = document.getElementById('promo_2');
+let Promo_3 = document.getElementById('promo_3');
+let Promo_4 = document.getElementById('promo_4');
+let Promo_5 = document.getElementById('promo_5');
+let totalPromo = document.getElementById('totalPromo');
+let promoTitle = document.getElementById('promoTitle');
+
+
+
+let biscuit155 = document.getElementById('biscuit155');
+let biscuit155_1 = document.getElementById('biscuit155_1');
+let biscuit155_2 = document.getElementById('biscuit155_2');
+let biscuit155_3 = document.getElementById('biscuit155_3');
+let biscuit155_4 = document.getElementById('biscuit155_4');
+let biscuit155_5 = document.getElementById('biscuit155_5');
+let totalBiscuit155 = document.getElementById('totalBiscuit155');
+let biscuit155Title = document.getElementById('biscuit155Title');
+
+
+
+
+let biscuit70 = document.getElementById('biscuit70');
+let biscuit70_1 = document.getElementById('biscuit70_1');
+let biscuit70_2 = document.getElementById('biscuit70_2');
+let biscuit70_3 = document.getElementById('biscuit0_3');
+let biscuit70_4 = document.getElementById('biscuit70_4');
+let biscuit70_5 = document.getElementById('biscuit70_5');
+let totalBiscuit70 = document.getElementById('totalBiscuit70');
+let biscuit70Title = document.getElementById('biscuit70Title');
+
+
+
+let marie110 = document.getElementById('marie110');
+let marie110_1 = document.getElementById('marie110_1');
+let marie110_2 = document.getElementById('marie110_2');
+let marie110_3 = document.getElementById('marie110_3');
+let marie110_4 = document.getElementById('marie110_4');
+let marie110_5 = document.getElementById('marie110_5');
+let totalMarie110 = document.getElementById('totalmarie110');
+let marie110Title = document.getElementById('marie110Title');
+
+
+
+let tourist = document.getElementById('tourist');
+let tourist_1 = document.getElementById('marie110_1');
+let tourist_2 = document.getElementById('marie110_2');
+let tourist_3 = document.getElementById('marie110_3');
+let tourist_4 = document.getElementById('marie110_4');
+let tourist_5 = document.getElementById('marie110_5');
+let totalTourist = document.getElementById('totalTourist');
+let touristTitle = document.getElementById('touristTitle');
+
+
+
+
+let kunuz = document.getElementById('kunuz');
+let kunuz_1 = document.getElementById('kunuz_1');
+let kunuz_2 = document.getElementById('kunuz_2');
+let kunuz_3 = document.getElementById('kunuz_3');
+let kunuz_4 = document.getElementById('kunuz_4');
+let kunuz_5 = document.getElementById('kunuz_5');
+let totalKunuz = document.getElementById('totalKunuz');
+let kunuzTitle = document.getElementById('kunuzTitle');
+
+
+
+
+
+
+
+one.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "marie60.jpg_480Wx480H" ;
+   
+    
+    
+}
+
+
+two.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "110.png" ;
+    mainCartoon.innerHTML = biscuit110_1.innerHTML;
+    mainBox.innerHTML = biscuit110_2.innerHTML;
+    mainDiscount.innerHTML = biscuit110_3.innerHTML;
+    mainPrice.innerHTML = biscuit110_4.innerHTML;
+    insideCrtoon.innerHTML = biscuit110_5.innerHTML;
+    totalDiscount.innerHTML = totalBiscuit110.innerHTML;
+    mainTitle.innerHTML = biscuit110Title.innerHTML;
+}
+
+
+
+three.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "wafer.jpeg" ;
+    mainCartoon.innerHTML = wafer_1.innerHTML;
+    mainBox.innerHTML = wafer_2.innerHTML;
+    mainDiscount.innerHTML = wafer_3.innerHTML;
+    mainPrice.innerHTML = wafer_4.innerHTML;
+    insideCrtoon.innerHTML = wafer_5.innerHTML;
+    totalDiscount.innerHTML = totalWafer.innerHTML;
+    mainTitle.innerHTML = waferTitle.innerHTML;
+}
+
+
+four.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "promo.jpg" ;
+    mainCartoon.innerHTML = promo_1.innerHTML;
+    mainBox.innerHTML = promo_2.innerHTML;
+    mainDiscount.innerHTML = promo_3.innerHTML;
+    mainPrice.innerHTML = promo_4.innerHTML;
+    insideCrtoon.innerHTML = promo_5.innerHTML;
+    totalDiscount.innerHTML = totalPromo.innerHTML;
+    mainTitle.innerHTML = promoTitle.innerHTML;
+}
+
+five.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "155box.jpg" ;
+    mainCartoon.innerHTML = biscuit155_1.innerHTML;
+    mainBox.innerHTML = biscuit155_2.innerHTML;
+    mainDiscount.innerHTML = biscuit155_3.innerHTML;
+    mainPrice.innerHTML = biscuit155_4.innerHTML;
+    insideCrtoon.innerHTML = biscuit155_5.innerHTML;
+    totalDiscount.innerHTML = totalBiscuit155.innerHTML;
+    mainTitle.innerHTML = biscuit155Title.innerHTML;
+}
+
+six.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "70.jpg" ;
+    mainCartoon.innerHTML = biscuit70_1.innerHTML;
+    mainBox.innerHTML = biscuit70_2.innerHTML;
+    mainDiscount.innerHTML = biscuit70_3.innerHTML;
+    mainPrice.innerHTML = biscuit70_4.innerHTML;
+    insideCrtoon.innerHTML = biscuit70_5.innerHTML;
+    totalDiscount.innerHTML = totalBiscuit70.innerHTML;
+    mainTitle.innerHTML = biscuit70Title.innerHTML;
+}
+
+
+seven.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "marie110.jpg" ;
+    mainCartoon.innerHTML = marie110_1.innerHTML;
+    mainBox.innerHTML = marie110_2.innerHTML;
+    mainDiscount.innerHTML = marie110_3.innerHTML;
+    mainPrice.innerHTML = marie110_4.innerHTML;
+    insideCrtoon.innerHTML = marie110_5.innerHTML;
+    totalDiscount.innerHTML = totalMarie110.innerHTML;
+    mainTitle.innerHTML = marie110Title.innerHTML;
+}
+
+
+eleven.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "tourist.jpg" ;
+    mainCartoon.innerHTML = tourist_1.innerHTML;
+    mainBox.innerHTML = tourist_2.innerHTML;
+    mainDiscount.innerHTML = tourist_3.innerHTML;
+    mainPrice.innerHTML = tourist_4.innerHTML;
+    insideCrtoon.innerHTML = tourist_5.innerHTML;
+    totalDiscount.innerHTML = totalTourist.innerHTML;
+    mainTitle.innerHTML = touristTitle.innerHTML;
+}
+
+
+
+thirteen.onclick = function () {
+    details.style.display = "block";
+    mainImage.src = "kunuz.jpg" ;
+    mainCartoon.innerHTML = kunuz_1.innerHTML;
+    mainBox.innerHTML = kunuz_2.innerHTML;
+    mainDiscount.innerHTML = kunuz_3.innerHTML;
+    mainPrice.innerHTML = kunuz_4.innerHTML;
+    insideCrtoon.innerHTML = kunuz_5.innerHTML;
+    totalDiscount.innerHTML = totalKunuz.innerHTML;
+    mainTitle.innerHTML = kunuzTitle.innerHTML;
+}
+
+
+
+
+
+
+
+
+
+let nine = document.getElementById('nine');
+
+nine.onclick = function (){
+    nine.innerHTML ="غير متوفر الان";
+    nine.style.backgroundColor = 'black';
+    nine.style.color = 'red';
+    nine.style.borderRadius = '10px';
+}
+
+
+let ten = document.getElementById('ten');
+
+ten.onclick = function (){
+    ten.innerHTML ="غير متوفر الان";
+    ten.style.backgroundColor = 'black';
+    ten.style.color = 'red';
+    ten.style.borderRadius = '10px';
+}
+
+let twoelve = document.getElementById('twoelve');
+
+twoelve.onclick = function (){
+    twoelve.innerHTML ="غير متوفر الان";
+    twoelve.style.backgroundColor = 'black';
+    twoelve.style.color = 'red';
+    twoelve.style.borderRadius = '10px';
+}
+
+
+
+
+
+
+
+let close = document.getElementById('close');
+
+close.onclick = function () {
+    details.style.display = 'none'
+}
+
